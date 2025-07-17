34f67ab3ac85eca91c176941ee17dc8e58639b69
Sharmeencodingal <sharmeenabdulwaheedd@gmail.com>
2025-07-17 16:19:50 +0500

    first module complete

--- inputs_variables.py
+++ inputs_variables.py
@@ -2,3 +2,17 @@ age=input("Enter your age: ")
 print(age)
 #Taking input from user using input key word
 #variable is container to hold data
+# Creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+
+# User Input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
